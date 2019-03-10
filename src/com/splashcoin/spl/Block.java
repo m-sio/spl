ab3922d78cb24f60bb62c2e2b548fd5b8c86ea6f
M-S.io <37083759+m-sio@users.noreply.github.com>
2019-03-10 12:29:26 +0000

    Set Block Variables

---
+++ src/com/splashcoin/spl/Block.java
@@ -0,0 +1,18 @@
+package com.splashcoin.spl;
+
+import java.math.BigInteger;
+
+public class Block {
+    /*
+        Set Block Variables;
+     */
+    protected int id;
+    protected String message;
+    protected String sender;
+    protected String receiver;
+    protected BigInteger hash;
+    protected BigInteger previousHash;
+    /*
+        End
+     */
+}
